20745b7594d0df804385149f28eb4b6b2193c997
xxoxxoyeom <jooyong34@gmail.com>
2020-04-13 14:41:54 +0900

    수정

--- src/table/MemberTable.java
+++ src/table/MemberTable.java
@@ -2,43 +2,48 @@ package table;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.Scrollable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import database.MemberDAO;
 import database.MemberVO;
 
-public class MemberTable extends JFrame {
+public class MemberTable extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
 	private JTextField txtName;
 	private JTextField txtAge;
 	private JTextField txtGender;
-	private JTextField textField;
+	private JTextField txtGetNo;
 	private JTable table;
-	private final JLabel lblNewLabel_5 = new JLabel("나이");
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTable table_1;
 	
 	private MemberDAO dao;
+	//전체조회
 	private DefaultTableModel model;
-
-	/**
-	 * Launch the application.
-	 */
+	//하나조회
+	private DefaultTableModel model1;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -55,28 +60,28 @@ public class MemberTable extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	/**
-	 * 
-	 */
 	public MemberTable() {
+		setTitle("회원정보");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 200);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		//DB 객체 생성
+		//DB객체 생성
 		dao = new MemberDAO();
+		
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
-		tabbedPane.addTab("회원 등록",null, panel, null);
+		tabbedPane.addTab("회원등록", null, panel, null);
 		panel.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		JLabel lblNewLabel = new JLabel("이름");
+		lblNewLabel.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 18));
 		panel.add(lblNewLabel);
 		
 		txtName = new JTextField();
@@ -84,6 +89,7 @@ public class MemberTable extends JFrame {
 		txtName.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("나이");
+		lblNewLabel_1.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 18));
 		panel.add(lblNewLabel_1);
 		
 		txtAge = new JTextField();
@@ -91,6 +97,7 @@ public class MemberTable extends JFrame {
 		txtAge.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("성별");
+		lblNewLabel_2.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 18));
 		panel.add(lblNewLabel_2);
 		
 		txtGender = new JTextField();
@@ -98,95 +105,151 @@ public class MemberTable extends JFrame {
 		txtGender.setColumns(10);
 		
 		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("회원 조회", null, panel_1, null);
+		tabbedPane.addTab("회원조회", null, panel_1, null);
 		panel_1.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_4 = new JPanel();
 		panel_1.add(panel_4, BorderLayout.NORTH);
 		
 		JLabel lblNewLabel_3 = new JLabel("회원번호");
+		lblNewLabel_3.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 18));
 		panel_4.add(lblNewLabel_3);
 		
-		textField = new JTextField();
-		panel_4.add(textField);
-		textField.setColumns(10);
+		txtGetNo = new JTextField();
+		panel_4.add(txtGetNo);
+		txtGetNo.setColumns(10);
 		
 		JButton btnNewButton = new JButton("조회");
-		btnNewButton.setToolTipText("");
 		panel_4.add(btnNewButton);
+		btnNewButton.addActionListener(this);
 		
-		table = new JTable();
-		panel_1.add(table, BorderLayout.CENTER);
+		model1 = getModel();
+		table = new JTable(model1);
+		JScrollPane scrollPane1 = new JScrollPane();
+		scrollPane1.setViewportView(table);
+		panel_1.add(scrollPane1, BorderLayout.CENTER);
 		
 		JPanel panel_2 = new JPanel();
-		tabbedPane.addTab("회원 수정", null, panel_2, null);
+		tabbedPane.addTab("회원수정", null, panel_2, null);
 		panel_2.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_5 = new JPanel();
-		panel_2.add(panel_5);
-		panel_5.setLayout(new BorderLayout(0, 0));
-		
 		JLabel lblNewLabel_4 = new JLabel("수정할 회원 정보 입력");
-		panel_5.add(lblNewLabel_4, BorderLayout.NORTH);
+		lblNewLabel_4.setFont(new Font("굴림", Font.BOLD, 18));
+		panel_2.add(lblNewLabel_4, BorderLayout.NORTH);
 		
-		JPanel panel_6 = new JPanel();
-		panel_5.add(panel_6, BorderLayout.CENTER);
-		panel_6.setLayout(new GridLayout(0, 2, 0, 0));
+		JPanel panel_5 = new JPanel();
+		panel_2.add(panel_5, BorderLayout.CENTER);
+		panel_5.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JLabel label = new JLabel("번호");
-		panel_6.add(label);
+		JLabel lblNewLabel_5 = new JLabel("번호");
+		lblNewLabel_5.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 18));
+		panel_5.add(lblNewLabel_5);
 		
 		textField_1 = new JTextField();
-		panel_6.add(textField_1);
+		panel_5.add(textField_1);
 		textField_1.setColumns(10);
-		panel_6.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("나이");
+		lblNewLabel_6.setFont(new Font("나눔고딕 ExtraBold", Font.PLAIN, 18));
+		panel_5.add(lblNewLabel_6);
 		
 		textField_2 = new JTextField();
-		panel_6.add(textField_2);
+		panel_5.add(textField_2);
 		textField_2.setColumns(10);
 		
 		JPanel panel_3 = new JPanel();
 		tabbedPane.addTab("회원삭제", null, panel_3, null);
 		
-		JLabel lblNewLabel_6 = new JLabel("회원번호");
-		panel_3.add(lblNewLabel_6);
+		JLabel lblNewLabel_7 = new JLabel("회원번호");
+		lblNewLabel_7.setFont(new Font("나눔고딕 ExtraBold", Font.BOLD, 18));
+		panel_3.add(lblNewLabel_7);
 		
 		textField_3 = new JTextField();
 		panel_3.add(textField_3);
 		textField_3.setColumns(10);
 		
-		JButton btnNewButton_1 = new JButton("삭제");
-		panel_3.add(btnNewButton_1);
+		JButton btnNewButton_2 = new JButton("삭제");
+		panel_3.add(btnNewButton_2);
 		
-		JPanel panel_7 = new JPanel();
-		tabbedPane.addTab("회원 전체 조회", null, panel_7, null);
-		panel_7.setLayout(new BorderLayout(0, 0));
+		JPanel panel_6 = new JPanel();
+		tabbedPane.addTab("회원전체조회", null, panel_6, null);
+		panel_6.setLayout(new BorderLayout(0, 0));
 		
 		JScrollPane scrollPane = new JScrollPane();
-		panel_7.add(scrollPane, BorderLayout.NORTH);
+		panel_6.add(scrollPane, BorderLayout.CENTER);
 		
-		// memberTBL의 전체 내용 자겨오기
+		//memberTBL의 전체 내용 가져오기		
+		table_1 = new JTable(getModel());
+		list();
+		scrollPane.setViewportView(table_1);	
+		
+		//회원등록 화면의 성별
+		txtGender.addActionListener(this);
+	}
+	
+	public DefaultTableModel getModel() {
 		String columnNames[]= {"번호","이름","나이","성별"};
 		model = new DefaultTableModel(columnNames,0) {
 			@Override
-			public boolean isCellEditable(int row, int column) {
+			public boolean isCellEditable(int row, int column) {			
 				return false;
-				
 			}
 		};
-		table_1 = new JTable(model);
-		list();
-		scrollPane.setViewportView(table_1);
+		return model;
 	}
 	
 	public void list() {
-		Vector<MemberVO> vecList = dao.getList();
-				
+		Vector<MemberVO> vecList=dao.getList();
+		
 		//vecList에 들어있는 값을 table에 보여주기
-		for(MemberVO vo:vecList=dao.getList()) {
-			Object[] objList = {vo.getNo(),vo.getName(),vo.getAge(),vo.getGender()};
+		for(MemberVO vo:vecList) {
+			Object[] objList= {vo.getNo(),vo.getName(),vo.getAge(),vo.getGender()};
 			model.addRow(objList);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==txtGender) {
+			//이름과 나이와 성별을 가져온 후 
+			MemberVO vo = new MemberVO();
+			vo.setName(txtName.getText());
+			vo.setAge(Integer.parseInt(txtAge.getText()));
+			vo.setGender(txtGender.getText());
+			
+			//데이터베이스에 입력하기
+			int result = dao.insert(vo);
+			
+			if(result>0) { //성공
+				JOptionPane.showMessageDialog(this, "입력성공");
+				//모델이 가지고 있었던 데이터 초기화
+				model.setNumRows(0);
+				list();
+			}else { //실패
+				JOptionPane.showMessageDialog(this, "입력실패");
+			}
+		}else if(e.getActionCommand().equals("조회")) {
+			//사용자가 입력한 번호 가져오기
+			int no = Integer.parseInt(txtGetNo.getText());
+			//번호에 해당하는 정보 가져온 후 보여주기
+			MemberVO vo = dao.getRow(no);
+			Object[] rowData = {vo.getNo(),vo.getName(),vo.getAge(),vo.getGender()};
+			model1.addRow(rowData);
 		}
 		
 	}
+	
+	// 1. push >> 원격저장소로 올리기 (github)
+	// 2. branch 
+
 }
+
+
+
+
+
+
+
+
+
